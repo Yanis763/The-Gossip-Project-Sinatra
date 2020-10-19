7dcf31fec7c230be094a9a9ce1654436bac21f8d
Yanis763 <yanispsn76@hotmail.com>
2020-10-19 22:44:03 +0200

    First Commit

---
+++ lib/gossip.rb
@@ -0,0 +1,22 @@
+require 'pry'
+require 'csv'
+class Gossip
+    attr_accessor :author, :content
+    def initialize(author, content)
+        @author = author
+        @content = content
+    end
+    def save
+        CSV.open("/home/yanis/Documents/THP/Ruby/the_gossip_project_sinatra/db/gossip.csv", "ab") do |csv|
+          csv << ["#{@author}, #{@content}"]
+        end
+    end
+    def self.all
+        all_gossips = []
+      
+        CSV.read("./db/gossip.csv").each do |csv_line|
+            all_gossips << Gossip.new(csv_line[0], csv_line[1])
+        end
+        return all_gossips
+      end
+end
